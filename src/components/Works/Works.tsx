cc5591298dd36f986f182be8b0e77d91385c15f2
coder6583 <thedude6583@gmail.com>
2023-10-21 02:00:05 -0400

    works fadein

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -1,35 +1,58 @@
+import { useEffect, useState } from "react";
 import Typewriter from "../Typewriter/Typewriter";
 import ImageItem from "./ImageItem";
 import styles from "./Works.module.css";
+import clsx from "clsx";
 
 export default function Works(): JSX.Element {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [firstAppear, setFirstAppear] = useState(false);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!firstAppear && scrollPosition > (window.innerHeight * 1) / 2) {
+      setFirstAppear(true);
+    }
+  }, [firstAppear, scrollPosition]);
   return (
     <div className={styles.parent}>
-      <div className={styles.projects}>
-        <div className={styles.row}>
-          <div className={styles.child}>
-            <ImageItem id="laze" />
+      {firstAppear && (
+        <div className={styles.projects}>
+          <div className={styles.row}>
+            <div className={clsx(styles.child, styles.child1)}>
+              <ImageItem id="laze" />
+            </div>
+            <div className={clsx(styles.child, styles.child2)}>
+              <ImageItem id="charEngine3D" />
+            </div>
+            <div className={clsx(styles.child, styles.child3)}>
+              <ImageItem id="engine3D" />
+            </div>
           </div>
-          <div className={styles.child}>
-            <ImageItem id="charEngine3D" />
-          </div>
-          <div className={styles.child}>
-            <ImageItem id="engine3D" />
-          </div>
-        </div>
-        <div className={styles.row}>
-          <div className={styles.child}>
-            <ImageItem id="textEditor" />
-          </div>
-          {/* <div className={styles.child}>
+          <div className={styles.row}>
+            <div className={clsx(styles.child, styles.child4)}>
+              <ImageItem id="textEditor" />
+            </div>
+            {/* <div className={styles.child}>
             <ImageItem id="laze" />
           </div>
           <div className={styles.child}>
             <ImageItem id="laze" />
             <div />
           </div> */}
-        </div>
-        {/* <div className={styles.row}>
+          </div>
+          {/* <div className={styles.row}>
           <div className={styles.child}>
             <ImageItem id="laze" />
           </div>
@@ -40,7 +63,8 @@ export default function Works(): JSX.Element {
             <ImageItem id="laze" />
           </div>
         </div> */}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
