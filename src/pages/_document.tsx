877ac900c77ce0aaaa4fc45b6d2f0af351521859
coder6583 <thedude6583@gmail.com>
2023-11-22 19:19:22 -0800

    updated font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;200;400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&family=JetBrains+Mono:wght@100;200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
       </Head>
